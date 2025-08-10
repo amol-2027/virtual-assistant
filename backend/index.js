9f7068a202393ebc0267e23bec249d9ea453ee1d
Amol_007 <ar9175995238@gmail.com>
2025-08-10 18:45:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-nyh5.onrender.com",
     credentials: true,
   })
 );
